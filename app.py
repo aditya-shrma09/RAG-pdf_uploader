374cfb964a36f4383de71e3b1df60c95e11be5cd
aditya-shrma09 <aditya.sharma.9.11.2004@gmail.com>
2026-06-08 13:55:00 +0530

    file upload feature added and a simple streamlit front end

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import streamlit as st
+import os
+
 from services.pdf_uploader import extract_text
 
-path = "sample.pdf"
+UPLOAD_DIR = "data/uploads"
 
-text = extract_text(path)
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-print(text)
+uploaded_file = st.file_uploader(
+    "Upload PDF",
+    type=["pdf"]
+)
+
+if uploaded_file:
+
+    file_path = os.path.join(
+        UPLOAD_DIR,
+        uploaded_file.name
+    )
+
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    text = extract_text(file_path)
+
+    st.write(text)

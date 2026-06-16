39ab5eee104f2744fb9b2864012fc77b7e01024b
aditya-shrma09 <aditya.sharma.9.11.2004@gmail.com>
2026-06-16 13:06:28 +0530

    top k chunks retrieval done for a question

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 from services.pdf_uploader import extract_text
 from services.chunker import chunker
 from services.embedding import embedder
+from services.retriver import create_vector_store
+from services.retriver import retrieve
 UPLOAD_DIR = "data/uploads"
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -22,22 +24,35 @@ if uploaded_file:
 
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-
     text = extract_text(file_path)
     chunks = chunker(text)
     vectors = embedder(chunks)
-   
+    vector_store = create_vector_store(chunks, vectors)
+    ques = st.text_input("Ask a question")
+    if ques:
 
-    st.write(vectors.shape)
-    st.write(vectors[0][:10])
-    for i, chunk in enumerate(chunks [:5]):
+        em_ques = embedder(ques)
 
-     st.subheader(f"Chunk {i + 1}")
+        top_k_chunks = retrieve(
+            em_ques,
+            vector_store
+        )
+        top_k_chunks = retrieve(em_ques,vector_store)
+        st.write(top_k_chunks)
 
-     st.text_area(
-         f"Chunk {i + 1}",
-        chunk,
-        height=200
-    )
+   # st.write(vectors.shape)
+    # st.write(vectors[0][:10])
+    # st.write("Number of entries:", len(vector_store))
+    # st.write(vector_store[0]["text"])
+    # st.write(len(vector_store[0]["embedding"]))
+    # for i, chunk in enumerate(chunks [:5]):
+
+    #  st.subheader(f"Chunk {i + 1}")
+
+    #  st.text_area(
+    #      f"Chunk {i + 1}",
+    #     chunk,
+    #     height=200
+    # )
      
     
--- services/retriver.py
+++ services/retriver.py
@@ -1,5 +1,5 @@
-
-
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 def create_vector_store(chunks, vectors):
 
     vector_store = []
@@ -12,4 +12,20 @@ def create_vector_store(chunks, vectors):
         })
 
     return vector_store
+
+def retrieve(equery, vector_store, k=3):
+     chunk_embeddings = [
+        item["embedding"]
+        for item in vector_store
+    ]
+     scores = cosine_similarity(
+        [equery],
+        chunk_embeddings
+    )[0]
+     top_indices = np.argsort(scores)[::-1][:k]
+     return [
+        vector_store[i]["text"]
+        for i in top_indices
+    ]
+
 

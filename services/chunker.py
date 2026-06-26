7682c1d78c791f25f829e7c3d7d80496b52ec7b4
aditya-shrma09 <aditya.sharma.9.11.2004@gmail.com>
2026-06-26 17:16:18 +0530

    updated chunker

--- services/chunker.py
+++ services/chunker.py
@@ -2,13 +2,13 @@ def chunker(text):
     chunk_size = 500
     overlap = 50
     start =0
-    #w = text.split()
+    w = text.split()
     chunks =[]
     while start < len(text):
 
         end = start + chunk_size
-
-        chunks.append(text[start:end])
+        chunk = " ".join(w[start:end])
+        chunks.append(chunk)
 
         start += chunk_size - overlap
     # for i in range (0,len(w),chunk_size):
